ce9de97f5733c83a7b133da91ab0b9daa9453b7f
Roche001 <rocheharold13@gmail.com>
2022-07-03 16:13:16 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -10,30 +10,33 @@ const Top = () => {
       <Navbar collapseOnSelect expand="lg" bg="#020c1b" variant="dark">
         <Container>
           <Navbar.Brand href="#Header">
-            <a href="#Home" className="office">
-              <h3 className="pharolix">
-                {" "}
-                <BsBuilding />
-                Pharolix
-              </h3>
-              <small className="agency">Real Estate Agency</small>
-            </a>
+            <div className="img">
+              <img
+                src="./assets/logo6.png"
+                alt="logo"
+                width="50px"
+                height="50px"
+              />
+            </div>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <div className="nav-items">
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#Header">
+                <Nav.Link href="#Home">
                   <h6>Home</h6>
                 </Nav.Link>
                 <Nav.Link href="#About">
                   <h6>About</h6>
                 </Nav.Link>
-                <Nav.Link href="#Experience">
-                  <h6>Experience</h6>
+                <Nav.Link href="#Agents">
+                  <h6>Agents</h6>
                 </Nav.Link>
-                <Nav.Link href="#Portfolio">
-                  <h6>Portfolio</h6>
+                <Nav.Link href="#Blogs">
+                  <h6>Blogs</h6>
+                </Nav.Link>
+                <Nav.Link href="#Reviews">
+                  <h6>Reviews</h6>
                 </Nav.Link>
                 <Nav.Link href="#Contacts">
                   <h6>Contacts</h6>
